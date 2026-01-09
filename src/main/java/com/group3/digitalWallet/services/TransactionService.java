18279d5da7734f7b3180f6f203786e1b15ffd335
Miguel Doménech <miguel.domenech@qaracter.com>
2026-01-09 10:51:30 +0100

    Implemented makeTransaction fully

--- src/main/java/com/group3/digitalWallet/services/TransactionService.java
+++ src/main/java/com/group3/digitalWallet/services/TransactionService.java
@@ -2,21 +2,28 @@ package com.group3.digitalWallet.services;
 
 import com.group3.digitalWallet.models.Currency;
 import com.group3.digitalWallet.models.Transaction;
+import com.group3.digitalWallet.models.User;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class TransactionService {
     private final List<Transaction> transactions = new ArrayList<>();
-
+    private final CurrencyConversionService currencyConversionService = new CurrencyConversionService();
     private TransactionService(){
     }
 
     // Returns true if the transaction was successful, false otherwise
-    private boolean makeTransaction(int userId1, Currency user1Currency,
-                                    int userId2, Currency user2Currency,
+    private boolean makeTransaction(User origUser, Currency origCurrency,
+                                    User destUser, Currency destCurrency,
                                     double amount){
-        return false; //TODO
+        if (amount < 0.0 || origUser.getBalance(origCurrency) < amount ){
+            return false;
+        }
+        origUser.withdraw(amount, origCurrency);
+        double convertedAmount = currencyConversionService.convert(amount, origCurrency, destCurrency);
+        destUser.deposit(convertedAmount, destCurrency);
+        return true;
     }
 
     public List<Transaction> getTransactions() {
