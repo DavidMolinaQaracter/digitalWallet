4759db9558d488e7d8a1a5ccf7b954fca9301349
Marina Garcia <marina.garcia@qaracter.com>
2026-01-09 09:23:50 +0000

    Controllers done

---
+++ src/main/java/com/group3/digitalWallet/controllers/UserController.java
@@ -0,0 +1,36 @@
+package com.group3.digitalWallet.controllers;
+import com.group3.digitalWallet.models.Currency;
+import com.group3.digitalWallet.models.User;
+import com.group3.digitalWallet.services.UserService;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody Map<String, String> request) {
+        return userService.createUser(request.get("name"));
+    }
+
+    @GetMapping
+    public List<User> getAll() {
+        return userService.getAllUsers();
+    }
+
+    // Endpoint para añadir saldo: /users/1/balance
+    @PostMapping("/{id}/balance")
+    public User addBalance(@PathVariable int id, @RequestBody Map<String, Object> request) {
+        Currency curr = Currency.valueOf(request.get("currency").toString().toUpperCase());
+        Double amount = Double.valueOf(request.get("amount").toString());
+        return userService.addBalance(id, curr, amount);
+    }
+}
