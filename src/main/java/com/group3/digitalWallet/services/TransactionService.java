5a44e640f399fec962adff4837ab471bfb8e4b33
Miguel Doménech <miguel.domenech@qaracter.com>
2026-01-09 11:15:13 +0100

    Declared CurrencyService as @Service, added saving transactions in TransactionService

--- src/main/java/com/group3/digitalWallet/services/TransactionService.java
+++ src/main/java/com/group3/digitalWallet/services/TransactionService.java
@@ -3,18 +3,26 @@ package com.group3.digitalWallet.services;
 import com.group3.digitalWallet.models.Currency;
 import com.group3.digitalWallet.models.Transaction;
 import com.group3.digitalWallet.models.User;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Service
 public class TransactionService {
     private final List<Transaction> transactions = new ArrayList<>();
-    private final CurrencyConversionService currencyConversionService = new CurrencyConversionService();
-    private TransactionService(){
+    private final CurrencyConversionService currencyConversionService;
+    private final UserService userService;
+    private int nextId;
+
+    private TransactionService(CurrencyConversionService currencyConversionService, UserService userService) {
+        nextId = 1;
+        this.currencyConversionService = currencyConversionService;
+        this.userService = userService;
     }
 
     // Returns true if the transaction was successful, false otherwise
-    private boolean makeTransaction(User origUser, Currency origCurrency,
+    public boolean makeTransaction(User origUser, Currency origCurrency,
                                     User destUser, Currency destCurrency,
                                     double amount){
         if (amount < 0.0 || origUser.getBalance(origCurrency) < amount ){
@@ -23,6 +31,9 @@ public class TransactionService {
         origUser.withdraw(amount, origCurrency);
         double convertedAmount = currencyConversionService.convert(amount, origCurrency, destCurrency);
         destUser.deposit(convertedAmount, destCurrency);
+        Transaction transaction = new Transaction(origUser.getId(), amount, origCurrency, destCurrency,
+                origUser.getId(), nextId++);
+        transactions.add(transaction);
         return true;
     }
 
